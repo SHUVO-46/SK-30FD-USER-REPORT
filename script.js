a138878c75008885bd99046d7cc6a99c492d5244
SK-SHUVOJIT <98234575+SHUVO-46@users.noreply.github.com>
2025-06-27 15:35:03 +0600

    Update script.js

--- script.js
+++ script.js
@@ -31,7 +31,7 @@ function processUID() {
     document.getElementById("back-uid").innerText = back.length;
     document.getElementById("duplicate-uid").innerText = duplicates;
 
-    const rate = ok.length < 50 ? 9.50 : 10.00;
+    const rate = ok.length < 50 ? 7.50 : 8.00;
     const amount = ok.length * rate;
     document.getElementById("amount").innerText = amount;
 
