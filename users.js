467238e4a6d50ac0beee01db810779c0d22676c1
SK-SHUVOJIT <98234575+SHUVO-46@users.noreply.github.com>
2025-08-16 11:22:03 +0600

    Update users.js

--- users.js
+++ users.js
@@ -1,21 +1,3 @@
-const users = [
-    { username: 'Monirul', password: 'Monirul@1', name: 'Monirul Islam' },
-    { username: 'prantomondal878', password: 'Pranto123', name: 'Pranto Mondal' },
-    { username: 'badhon29', password: 'bs2949', name: 'Badhon Das' },
-    { username: 'partho0000', password: 'Partho000', name: 'Partho Sarkar' },
-    { username: 'admin', password: 'admin', name: 'ADMIN' },
-    { username: 'Samiran', password: 'Samiran60', name: 'Samiran Mondal' },
-    { username: 'joy12345', password: '12341234', name: 'JOY' },
-    { username: 'Bappy0203', password: 'Bappy2003', name: 'Ariyan Bappy' },
-    { username: 'Monir1051', password: 'Monir2025', name: 'MD Monir' },
-    { username: 'rana120', password: '@@rana12', name: 'Rana Vai' },
-    { username: 'Dweep', password: '501135', name: 'Dweep' },
-    { username: 'Saurav Roy', password: 'Saurav', name: 'Saurav Roy' },
-    { username: 'shaon707', password: 'shaon@', name: 'Shaon Kumar' },
-    { username: '@apurbo2122', password: 'Apurbo2122', name: 'Apurbo Sarkar' },
-    { username: 'vkhridoy', password: 'vkhridoy4444', name: 'Hridoy Mondal' },
-];
-
 const okUIDs = ["61578826846260",
 "61578886576722",
 "61578978527105",
@@ -148,6 +130,7 @@ const okUIDs = ["61578826846260",
 "61579630550585",
 "61579634660818",
 "61579641830451"];
+
 
 
 
